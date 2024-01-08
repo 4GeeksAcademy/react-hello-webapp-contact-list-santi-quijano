7ffb5927e1bcc90827f1b56d7dca9035bca029ae
Santi-Quijano <108496719+Santi-Quijano@users.noreply.github.com>
2024-01-08 00:48:31 +0000

    Done functionalities

--- src/js/views/PostContact.js
+++ src/js/views/PostContact.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useContext, useState} from 'react';
+import { Context } from '../store/appContext';
 import "../../styles/PostContact.css";
 
 const PostContact = () => {
