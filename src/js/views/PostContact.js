98b8abcd50264e3f51c4d6a283f5e0fe23412acc
Santi-Quijano <108496719+Santi-Quijano@users.noreply.github.com>
2024-01-08 01:31:49 +0000

    It works, somehow

--- src/js/views/PostContact.js
+++ src/js/views/PostContact.js
@@ -1,12 +1,46 @@
-import React, {useContext, useState} from 'react';
+import React, { useContext, useState } from 'react';
 import { Context } from '../store/appContext';
 import "../../styles/PostContact.css";
 
 const PostContact = () => {
+  const { store, actions } = useContext(Context);
+
+  const [full_name, setFull_name] = useState("");
+  const [address, setAddress] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleNewContact = async (e) => {
+    e.preventDefault();
+    actions.getContacts();
+    actions.postContact(full_name, address, phone, email);
+  }
+
   return (
-	<div>
-	  
-	</div>
+    <div className="container mt-10">
+      <h1 className="text-center">Add contact</h1>
+      <form onSubmit={handleNewContact}>
+        <div className="mb-2">
+          <p>Full name</p>
+          <input type="text" className="form-control" placeholder="Full Name" value={full_name} onChange={(e) => setFull_name(e.target.value)} />
+        </div>
+        <div className="mb-2">
+          <p>Email</p>
+          <input type="email" className="form-control" id="inputEmail" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </div>
+        <div className="mb-2">
+          <p>Phone</p>
+          <input type="tel" className="form-control" id="inputPhone" placeholder="Enter phone" onChange={(e) => setPhone(e.target.value)} />
+        </div>
+        <div className="mb-2">
+          <p>Address</p>
+          <input type="text" className="form-control" id="inputAddress" placeholder="Enter address" value={address} onChange={(e) => setAddress(e.target.value)} />
+        </div>
+        <button type="submit" className="btn btn-primary w-100">Save</button>
+      </form>
+      <br />
+      <Link to="/"><button type="submit" className="btn btn-primary w-100">Get back to contacts</button></Link>
+    </div>
   )
 }
 
--- src/js/views/PutContact.js
+++ src/js/views/PutContact.js
@@ -1,10 +1,59 @@
-import React from 'react'
+import React, { useState, useEffect, useContext } from 'react';
 
-const PutContact = () => {
+const PutContact = ({ contact_id }) => {
+
+  const { store, actions } = useContext(Context);
+
+  const [full_name, setFull_name] = useState("");
+  const [address, setAddress] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleUpdateOfContact = (e) => {
+    e.preventDefault();
+    actions.getContacts();
+    actions.putContact(contact_id, full_name, address, phone, email);
+  }
+
+  useEffect(() => {
+    handleContactInfo();
+  })
+
+  const handleContactInfo = async () => {
+    const getAContact = await actions.getContact(contact_id);
+
+    setFull_name(getAContact.full_name)
+    setAddress(getAContact.address)
+    setPhone(getAContact.phone)
+    setEmail(getAContact.email)
+  };
+
+
   return (
-	<div>
-	  
-	</div>
+    <div className="container mt-10">
+      <h1 className="text-center">Add contact</h1>
+      <form onSubmit={handleUpdateOfContact}>
+        <div className="mb-2">
+          <p>Full name</p>
+          <input type="text" className="form-control" placeholder="Full Name" value={full_name} onChange={(e) => setFull_name(e.target.value)} />
+        </div>
+        <div className="mb-2">
+          <p>Email</p>
+          <input type="email" className="form-control" id="inputEmail" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </div>
+        <div className="mb-2">
+          <p>Phone</p>
+          <input type="tel" className="form-control" id="inputPhone" placeholder="Enter phone" onChange={(e) => setPhone(e.target.value)} />
+        </div>
+        <div className="mb-2">
+          <p>Address</p>
+          <input type="text" className="form-control" id="inputAddress" placeholder="Enter address" value={address} onChange={(e) => setAddress(e.target.value)} />
+        </div>
+        <button type="submit" className="btn btn-primary w-100">Save</button>
+      </form>
+      <br />
+      <Link to="/"><button type="submit" className="btn btn-primary w-100">Get back to contacts</button></Link>
+    </div>
   )
 }
 
